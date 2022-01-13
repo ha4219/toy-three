a349fcb4b604863aebeddf721b9dbabb8fcb2613
ha4219 <ha4219@naver.com>
2022-01-13 16:03:56 +0900

    [ha4219] edit

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,25 +14,10 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        <Container maxWidth="md">
-          <h1 className={styles.title}>
-            Welcome to <a href="https://nextjs.org">Next.js!</a>
-          </h1>
+        <Container maxWidth="sm" sx={{ height: "100%" }}>
+          <h1 className={styles.title}>dongha~</h1>
         </Container>
       </Layout>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 };
